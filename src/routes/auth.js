d978f1f47702171adfa00dc5caa6861aa5719731
Giancarlo Joel Alvarez Gallardo <galvarezga21@gmail.com>
2025-05-29 22:11:06 -0500

    Correccion backend

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-// const { check, validationResult } = require('express-validator');
+const { check } = require('express-validator');//validationResult
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const authMiddleware = require('../middleware/authMiddleware');
